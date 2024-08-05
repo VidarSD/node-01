e4f28925c88937a01961f696da3fa31f0ac5f0b7
VidarSD <gutierrezcardenasjesus@gmail.com>
2024-08-05 09:20:30 -0600

    src

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const frutas = require('./frutas')
+
+frutas.forEach((fruta,index) => {
+
+    console.log('@@@ fruta =>',index,fruta)
+})
---
+++ src/frutas.js
@@ -0,0 +1,7 @@
+const frutas = ['platano','guanabana','maracuya','kiwi','pitaya']
+
+//frutas.forEach((fruta) => {
+//    console.log('@@@ fruta => ',fruta)
+//})
+
+module.exports = frutas
